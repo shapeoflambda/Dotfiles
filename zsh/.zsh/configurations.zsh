8b4b778e77b1685c40938a88707286ab1586049e
Harish C <shapeoflambda@gmail.com>
2019-09-22 18:04:08 +0530

    Fix vi mode in the shell

--- zsh/.zsh/configurations.zsh
+++ zsh/.zsh/configurations.zsh
@@ -62,10 +62,34 @@ bindkey '^[[3;5~' backward-delete-word
 bindkey '^[[A' up-line-or-search
 bindkey '^[[B' down-line-or-search
 
-# Open vi to edit the command on pressing <C-x><C-e>
-autoload -U edit-command-line
-zle -N edit-command-line
-bindkey -M vicmd v edit-command-line
+# vi mode
+bindkey -v
+export KEYTIMEOUT=1
+
+# Change cursor shape for different vi modes.
+function zle-keymap-select {
+  if [[ ${KEYMAP} == vicmd ]] ||
+     [[ $1 = 'block' ]]; then
+    echo -ne '\e[1 q'
+  elif [[ ${KEYMAP} == main ]] ||
+       [[ ${KEYMAP} == viins ]] ||
+       [[ ${KEYMAP} = '' ]] ||
+       [[ $1 = 'beam' ]]; then
+    echo -ne '\e[5 q'
+  fi
+}
+zle -N zle-keymap-select
+zle-line-init() {
+    zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
+    echo -ne "\e[5 q"
+}
+zle -N zle-line-init
+echo -ne '\e[5 q' # Use beam shape cursor on startup.
+preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
+
+# Edit line in vim with ctrl-e:
+autoload edit-command-line; zle -N edit-command-line
+bindkey '^e' edit-command-line
 
 # # search history with fzf if installed, default otherwise
 # For Arch
