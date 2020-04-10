7bcd44b3fd008ae8880bc6699976f1e036112ae8
Harish Chandran <chndha@amazon.com>
2020-04-10 19:56:09 +0530

    add color to man pages

--- zsh/.zsh/plugins.zsh
+++ zsh/.zsh/plugins.zsh
@@ -7,3 +7,5 @@ zplugin light zsh-users/zsh-autosuggestions
 zplugin light zdharma/fast-syntax-highlighting
 
 zplugin compinit >> /dev/null
+
+zinit snippet OMZ::plugins/colored-man-pages/colored-man-pages.plugin.zsh
