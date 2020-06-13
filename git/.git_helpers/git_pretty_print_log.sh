7f6f10553e2f270f19f1753b5900a28671910b77
shapeoflambda <shapeoflambda@gmail.com>
2020-06-13 22:58:42 +0530

    Fixed whitespace error in the git log script

--- git/.git_helpers/git_pretty_print_log.sh
+++ git/.git_helpers/git_pretty_print_log.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-HASH="%C(always,cyan)%h%C(always,reset)"             # Abbreviated hash
-DATE="%C(always,green)%as%C(always,reset)"           # Short format of date
-REF="%C(always,red)%D%C(always,reset)"               # Ref
-AUTHOR_NAME="%C(always,yellow)%an%C(always,reset)"   # Author name
-MESSAGE="%s"                                         # Commit subject
+HASH="%C(always,cyan)%h%C(always,reset)"           # Abbreviated hash
+DATE="%C(always,green)%as%C(always,reset)"         # Short format of date
+REF="%C(always,red)%D%C(always,reset)"             # Ref
+AUTHOR_NAME="%C(always,yellow)%an%C(always,reset)" # Author name
+MESSAGE="%s"                                       # Commit subject
 
-FORMAT="$HASH{$DATE{$AUTHOR_NAME{$REF $MESSAGE"
+FORMAT="$HASH{$DATE{$AUTHOR_NAME{$REF$MESSAGE"
 
 git_pretty_print_log() {
   git log --graph --pretty=tformat:$FORMAT $* |
-  column -t -s '{' |
-  less -FXRS
+    column -t -s '{' |
+    less -FXRS
 }
